8898a8ff278a9a5d579a6ebfac19d19aa89f85d2
Yuri Macedo Rezende Quintanilha <ymquint@gmail.com>
2020-05-06 14:33:47 -0300

    refactor: Changed the start text

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,6 +17,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!\n')
+    print('Let\'s start_time!\n')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = ''
     while city == '' or city.lower() != 'Chicago'.lower() or city.lower() != 'New York'.lower() or city.lower() != 'Washington'.lower():
